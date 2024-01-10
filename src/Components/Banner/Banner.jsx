4e0d122bbc9e48dfbcea526a0c6a976c3b6e705b
pronoyNath <pronoynath890@gmail.com>
2024-01-10 19:33:04 +0600

    home page updated

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,6 +1,7 @@
 import Lottie from 'lottie-react';
 import React from 'react';
 import bannerAnimation from "../../assets/banner-animation.json"
+import rightArrow from "../../assets/rightArrow.json"
 
 const Banner = () => {
     return (
@@ -8,6 +9,11 @@ const Banner = () => {
             <h3 className='text-4xl text-center font-bold my-10'><span className='text-[#3fa92b]'>Welcome To, </span> Time Tracker Web Appication</h3>
             <div className='max-w-6xl mx-auto'>
             <Lottie animationData={bannerAnimation}></Lottie>
+            </div>
+
+            <div className='text-3xl text-center my-10 p-5 font-bold w-full max-w-6xl bg-[#3fa92b] text-white mx-auto rounded flex items-center justify-center gap-3  hover:shadow-2xl hover:shadow-green-200 hover:transition hover:ease-in-out duration-1000 hover:delay-150 hover:translate-l-9 hover:rotate-3 '>
+                <h3 className=''>Track Your Time</h3>
+                <Lottie animationData={rightArrow} className='w-[100px] h-50px'></Lottie>
             </div>
         </div>
     );
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Navbar from '../../Components/Navbar/Navbar';
 import Banner from '../../Components/Banner/Banner';
+import Footer from '../../Components/Footer/Footer';
 
 const Home = () => {
     return (
         <div className='font-poppins'>
             <Navbar></Navbar>
             <Banner></Banner>
+            <Footer></Footer>
         </div>
     );
 };
